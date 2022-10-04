3a12106fa355ca491dd341244afaae5ea802ed60
DESKTOP-50B3PKI\anubu <a.wolfus@gmail.com>
2022-10-04 13:43:27 +0200

    Finished basic styling of articles, create, edit and error msgs

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,13 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+        });
+    });
+});
