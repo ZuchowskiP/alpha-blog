6ad7d6e788d737e10bced77d20f1960e0c6bc121
DESKTOP-50B3PKI\anubu <a.wolfus@gmail.com>
2022-10-07 12:57:47 +0200

    Added user page with articles made by user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def gravitar_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "ml-auto mr-auto image is-128x128")
+  end
 end
